b2eafe7998caf7bbc410fd2d8733a9d1ec676bbd
Manish-Kharagjitsing <mk93.empire@gmail.com>
2021-11-10 16:36:42 +0100

    Test file and Redshift cluster creation file

---
+++ create_red_cluster.py
@@ -0,0 +1,57 @@
+import boto3
+
+def clients():
+    ec2 = boto3.resource('ec2',
+                        region_name="us-west-2",
+                        aws_access_key_id=KEY,
+                        aws_secret_access_key=SECRET
+                    )
+
+    s3 = boto3.resource('s3',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                   )
+
+    iam = boto3.client('iam',
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET,
+                       region_name='us-west-2'
+                  )
+
+    redshift = boto3.client('redshift',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                  )
+
+    
+
+def create_red_clust():
+    try:
+        response = redshift.create_cluster(
+            ClusterType=DWH_CLUSTER_TYPE,
+            NodeType=DWH_NODE_TYPE,
+            NumberOfNodes=int(DWH_NUM_NODES),
+            #Identifiers & Credentials
+            DBName=DWH_DB,
+            ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+            MasterUsername=DWH_DB_USER,
+            MasterUserPassword=DWH_DB_PASSWORD,
+            #Roles (for s3 access)
+            IamRoles=[roleArn]  
+        )
+    except Exception as e:
+        print(e)
+
+    pass
+
+
+def main():
+    clients()
+    create_red_clust()
+    pass
+
+
+if __name__=="__main__":
+    main()
--- test.py
+++ test.py
@@ -15,8 +15,17 @@ Test
 
 
 def main():
-
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+    try:
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        cur = conn.cursor()
+    except Exception as e:
+        print(e)
+        print("Connection Not Established")
     pass
+
+
 
 if __name__=="__main__":
     main()
