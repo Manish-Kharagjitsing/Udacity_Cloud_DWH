6c7b8dcec4735dbb2c97033a8e68233c7511c284
Manish-Kharagjitsing <mk93.empire@gmail.com>
2021-11-10 11:31:36 +0100

    test.py added

---
+++ test.py
@@ -0,0 +1,22 @@
+import configparser
+import psycopg2
+
+
+"""
+Test Connections
+"""
+def aws_connect():
+    pass
+
+
+"""
+Test 
+"""
+
+
+def main():
+
+    pass
+
+if __name__=="__main__":
+    main()
